9eb2159411cb96530f17f2016854b1276ce35317
Gbissell93 <gregorybisselledu@gmail.com>
2021-07-26 12:13:48 -0400

    metronome and count working

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
-
+const tock = new Audio('sounds/tock.mp3');
+let beat = 0;
+const count = document.querySelector('.count')
 // This function is called every 600ms
 function update() {
-
-    // Play the 'tick' sound
-    tick.play();
+    if (beat === 3){
+        tock.play();
+        }
+        else {
+        tick.play();
+        }
+    beat++
+    if (beat === 4) {
+        beat = 0
+    }
+console.log(beat)
+count.innerText = beat + 1
 }
 
 // This function sets up update() to be called every 600ms
