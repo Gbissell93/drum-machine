bb91e1b098c2bdfd5788a978b81a5c13d6aa916a
Gbissell93 <gregorybisselledu@gmail.com>
2021-07-27 14:49:26 -0400

    added simple beat grid instead of text input for timing

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const count = document.querySelector('.count')
 
 // This function is called every 600ms
 function update() {
+    //beat increment
     beat++
     //instrument section
     const metronome = document.querySelector('#metro');
@@ -20,9 +21,20 @@ function update() {
     const hatCheck = document.querySelector('#hi-hat');
 
     //instrument timing selectors
-    const kickTime = document.querySelector('#kick-timing');
-    const snareTime = document.querySelector('#snare-timing');
-    const hatTime = document.querySelector('#hi-hat-timing');
+    //Kicks===============================================
+    const kick1 = document.querySelector('#kick-1');
+    const kick2 = document.querySelector('#kick-2');
+    const kick3 = document.querySelector('#kick-3');
+    const kick4 = document.querySelector('#kick-4');
+    const snare1 = document.querySelector('#snare-1');
+    const snare2 = document.querySelector('#snare-2');
+    const snare3 = document.querySelector('#snare-3');
+    const snare4 = document.querySelector('#snare-4');
+    const hiHat1 = document.querySelector('#hi-hat-1');
+    const hiHat2 = document.querySelector('#hi-hat-2');
+    const hiHat3 = document.querySelector('#hi-hat-3');
+    const hiHat4 = document.querySelector('#hi-hat-4');
+    
     //play conditions
     if (metronome.checked === true)
     {
@@ -34,19 +46,46 @@ function update() {
             tick.play();
             }
         }
-        
-        if (kickCheck.checked === true && Number(kickTime.value) === beat  ) {
+        //Kick conditions
+        if ( kick1.checked === true && Number(kick1.value) === beat) {
             kick.play()
         }
-        if (snareCheck.checked === true && Number(snareTime.value) === beat) {
-            snare.play()
+        if ( kick2.checked === true && Number(kick2.value) === beat) {
+            kick.play()
         }
-        if (hatCheck.checked === true && Number(hatTime.value) === beat ) {
-            hiHat.play()
+        if ( kick3.checked === true && Number(kick3.value) === beat) {
+            kick.play()
+        }
+        if ( kick4.checked === true && Number(kick4.value) === beat) {
+            kick.play()
         }
 
-        
-        
+        if ( snare1.checked === true && Number(snare1.value) === beat) {
+            snare.play()
+        }
+        if ( snare2.checked === true && Number(snare2.value) === beat) {
+            snare.play()
+        }
+        if ( snare3.checked === true && Number(snare3.value) === beat) {
+            snare.play()
+        }
+        if ( snare4.checked === true && Number(snare4.value) === beat) {
+            snare.play()
+        }
+    
+        if ( hiHat1.checked === true && Number(hiHat1.value) === beat) {
+            hiHat.play()
+        }
+        if ( hiHat2.checked === true && Number(hiHat2.value) === beat) {
+            hiHat.play()
+        }
+        if ( hiHat3.checked === true && Number(hiHat3.value) === beat) {
+            hiHat.play()
+        }
+        if ( hiHat4.checked === true && Number(hiHat4.value) === beat) {
+            hiHat.play()
+        }
+    
         //global timing
         count.innerText = beat 
         if (beat == 4) {
